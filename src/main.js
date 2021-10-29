34726f0d788eceb80c9b7a42f13527040cbf7e67
Gilbert Ngeno <ngenogilbert07@gmail.com>
2021-10-29 10:35:50 +0300

    api integration

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import tachyons/css/tachyons.css
 
 createApp(App).mount('#app')
+
+import router from './router'
+ new Vue({
+  router,
+  render: h => h(App),
+ }).$mount('#app')
+
+ import Cloudinary from "cloudinary-vue";
+ Vue.use(Cloudinary, {
+  configuration: { 
+   cloudName: "bomet",
+   secure: true 
+  }
+ });
